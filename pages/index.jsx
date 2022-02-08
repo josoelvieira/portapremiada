c1ca5e6878683df756981d2f9c13ed72674018e9
Josoel <cvl.jv1@gmail.com>
2022-02-08 20:41:26 -0300

    criando os componets e o obejto porta

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,15 @@
+import { useState } from "react";
 import Porta from "../componets/Porta";
 import Presente from "../componets/Presente";
+import PortaModel from "../model/porta";
 
 export default function Home() {
+  const [p1, setP1] = useState(new PortaModel(1));
+
   return (
-    <div>
-      <Porta />
+    <div style={{display:'flex'}}>
+      <Porta porta={p1} />
+      <Porta porta={p2} />
     </div>
   )
 }
